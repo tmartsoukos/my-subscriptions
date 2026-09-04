b91c1dac22453b0437adf9636e4355389bb03ed5
tmartsoukos <themismrtsk@gmail.com>
2026-09-04 13:09:42 +0300

    Το μάτι δίπλα στις ρυθμίσεις, όχι στη μέση της μπάρας

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "dash-v28";
+const CACHE = "dash-v29";
 const ASSETS = [
   "./", "index.html", "manifest.json", "icon-192.png", "icon-512.png", "icon-income.png", "icon-expense.png",
   "css/app.css",
